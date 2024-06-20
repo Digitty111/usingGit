3f9a1664dc12fbd0be1cdf924a660aacbac1c998
Joseph Anamene <joseph.anamene@c-ileasing.com>
2024-06-20 20:05:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-green-300 underline">
+      learning to use Git commit and version control with React and Vite  
+    </h1>
+  )
+}
+
+export default App
